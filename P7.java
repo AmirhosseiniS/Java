837637f2224525341baf0d0507ccb585c1e46274
AmirhosseiniS <saharamh21@gmail.com>
2025-08-14 00:47:54 +0330

    Add files via upload

---
+++ P7.java
@@ -0,0 +1,18 @@
+public class P7 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int number = 2; number < 1000; number++) {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                sum += number;
+            }
+        }
+        System.out.println("The sum of all prime numbers between 0 and 1000 is:" + sum);
+    }
+}

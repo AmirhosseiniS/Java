2caff9dd2343015748cf6104d6caad1d72a173f6
AmirhosseiniS <saharamh21@gmail.com>
2025-08-14 16:02:35 +0330

    Add files via upload

---
+++ P9.java
@@ -0,0 +1,13 @@
+public class P9 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");}
+            System.out.println();
+        }
+        for (int k = 5; k >= 1; k--) {
+            for (int j = 1; j <= k; j++) {
+                System.out.print("*");}
+            System.out.println();
+        }}}
+
